463a446f7bd1b6ce66a08813c61d83177860f059
ValeraPo <77934608+ValeraPo@users.noreply.github.com>
2021-12-17 23:47:43 +0300

    Add files via upload

---
+++ Teacher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace UltimateTests.Logics
+{
+    public class Teacher : IUser
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public List<string> Groups { get; set; } // Группы, которые курирует
+    }
+}
