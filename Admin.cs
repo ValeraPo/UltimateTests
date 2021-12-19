9a82292a3a3ba668d5a26a4c4b45996b95db881d
ValeraPo <portnovavalerija@mail.ru>
2021-12-19 13:39:13 +0300

    dtrydr

--- Admin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UltimateTests.Logics
-{
-    public class Admin : IUser
-    {
-        public string Login { get; set; }
-        public string Name { get; set; }
-        
-    }
-}
--- IUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UltimateTests.Logics
-{
-    public interface IUser
-    {
-        public string Login { get; set; }
-        public string Name { get; set; }
-        
-    }
-}
