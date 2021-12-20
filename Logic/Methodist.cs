967fe608bf88ef13adf08630728a4e90d50a217c
ValeraPo <portnovavalerija@mail.ru>
2021-12-20 18:03:31 +0300

    Change class Quize

--- Logic/Methodist.cs
+++ Logic/Methodist.cs
@@ -6,6 +6,10 @@ namespace Logic
     {
         public string Login { get; set; }
         public string Name { get; set; }
+        public string Hash { get; set; }
+        public string ID { get; set; }
+
         public List<Quize> Quizes { get; set; } // Тесты которые создал
+
     }
 }
