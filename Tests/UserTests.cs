5e5c65dc852146505064f6a2f983baee2a0aa196
DF\sovaev <evgeniy.sovalev@f-secure.com>
2022-01-09 17:35:33 +0300

    adding tests for user

---
+++ Tests/UserTests.cs
@@ -0,0 +1,25 @@
+using Logic.Configuration;
+using Logic.Interfaces;
+using Logic.Processes;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class UserTests
+    {
+        private IUser _user = IocKernel.Get<IUser>();
+
+        [Test]
+        public void AuthorizationNegativeTest()
+        {
+            Assert.Throws<System.InvalidOperationException>(() => _user.Authorization("login", "password"));
+        }
+        [Test]
+        public void AddNewUserNegativeTest()
+        {
+            //TODO дописать существующие логин и емайл
+            Assert.Throws<System.InvalidOperationException>(()
+                => _user.AddNewUser("Владимир Путин", "blabla", "blabla", "blabla", 1));
+        }
+    }
+}
